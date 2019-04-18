3b34d2c8cbffce5192cfe5f64f2ca5223c152b31
mjtillman <mjtillman@gmail.com>
2019-04-18 14:44:12 -0500

    Added sessions-and-cookies-exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,18 +7,32 @@ import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Hello World");
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+
+        System.out.println(username);
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            //        Cookie loginCookie = new Cookie("login", null);
+
+//   In your LoginServlet, when a user successfully logs in, set a key named "user" in the session.
+            request.getSession().setAttribute("user", username);
+            request.getSession().setAttribute("loggedIn", true);
+            System.out.println(request.getSession().getAttribute("user"));
+            System.out.println("Logged in: " + request.getSession().getAttribute("loggedIn"));
             response.sendRedirect("/profile");
         } else {
+            request.getSession().setAttribute("loggedIn", false);
+            System.out.println("Logged in: " + request.getSession().getAttribute("loggedIn"));
+            System.out.println("Do not pass Go, do not collect $200");
             response.sendRedirect("/login");
         }
     }
--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,6 +8,24 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
+//   Ensure that users can only visit the /profile page if they are logged in.
+//        System.out.println("In ViewProfileServlet:");
+//        System.out.println("Logged in: " + request.getSession().getAttribute("loggedIn"));
+//
+//        System.out.println(request.getSession().getAttribute("loggedIn"));
+
+        boolean loggedIn = (Boolean) request.getSession().getAttribute("loggedIn");
+
+        if (loggedIn) {
+            request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+//
+//
+//        System.out.println(request.getSession().getAttribute("user"));
+
+
     }
 }
